21df54b51dfb8f5b5c228d168c55016421aaa7d7
vamsi-273 <kvamsikrishnavarma@gmail.com>
2026-04-10 10:19:53 +0530

    Function to fetch Venues data added

--- src/main/java/com/ipl/automation/utils/TestData.java
+++ src/main/java/com/ipl/automation/utils/TestData.java
@@ -1,10 +1,15 @@
 package com.ipl.automation.utils;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.apache.logging.log4j.core.util.JsonUtils;
 
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestData {
 
@@ -19,7 +24,6 @@ public class TestData {
         }
     }
 
-    // 🔹 OLD FUNCTION (now reads from JSON)
     public static String[] getTeamNames() {
 
         JsonArray teams = data.getAsJsonArray("teams");
@@ -32,7 +36,6 @@ public class TestData {
         return teamNames;
     }
 
-    // 🔹 NEW FUNCTIONS
     public static String getTopTeamName() {
         return data.getAsJsonObject("topTeam").get("name").getAsString();
     }
@@ -43,5 +46,28 @@ public class TestData {
 
     public static int getTopTeamPoints() {
         return data.getAsJsonObject("topTeam").get("points").getAsInt();
+    }
+
+    public static List<String> getVenuesFromJson() {
+
+        List<String> venues = new ArrayList<>();
+
+        try {
+            InputStreamReader reader = new InputStreamReader(
+                    JsonUtils.class.getClassLoader()
+                            .getResourceAsStream("venues.json"));
+
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+            JsonArray jsonArray = jsonObject.getAsJsonArray("venues");
+
+            for (JsonElement element : jsonArray) {
+                venues.add(element.getAsString().trim());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return venues;
     }
 }
